52ffbf7ddeb30f22f052b37e0a125482f9cb41a6
georgiev-ilian <georgiev.ilian@gmail.com>
2021-06-13 00:16:35 +0300

    Change project licence to GPLv3

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,19 @@
+rem Copyright (C) 2021 Iliyan Georgiev
+rem 
+rem This file is part of yellowAI.
+rem 
+rem yellowAI is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem 
+rem yellowAI is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem 
+rem You should have received a copy of the GNU General Public License
+rem along with yellowAI.  If not, see <http://www.gnu.org/licenses/>.
 
 copy main.nut E:\games\OpenTTD\ai\yellowAI\
 copy info.nut E:\games\OpenTTD\ai\yellowAI\
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Copyright (C) 2021 Iliyan Georgiev
+# 
+# This file is part of yellowAI.
+# 
+# yellowAI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# yellowAI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with yellowAI.  If not, see <http://www.gnu.org/licenses/>.
+
 cp main.nut ~/.openttd/content_download/ai/yellowAI/
 cp info.nut ~/.openttd/content_download/ai/yellowAI/
 
